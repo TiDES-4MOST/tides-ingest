f643fe5de4d6313676eed3de6128516e3be2bf21
chrisfrohmaier <cf5g09@soton.ac.uk>
2026-01-10 18:24:28 +0000

    Updating some SQL taks

--- sql_tasks/deactivateUnobserved.sql
+++ sql_tasks/deactivateUnobserved.sql
@@ -1,4 +1,13 @@
-CREATE TEMP TABLE to_deactivate AS SELECT * from tides_master where active=True and updated < now() - interval '4days' or (rmag>20.5 and gmag>20.5);
-
-UPDATE tides_master SET active=False FROM to_deactivate WHERE to_deactivate.tides_id=tides_master.tides_id;  
-
+CREATE TEMP TABLE to_deactivate AS
+SELECT *
+FROM tides_master
+WHERE active = True
+    AND updated < now() - interval '5days'
+    OR (
+        rmag > 22.5
+        and gmag > 22.5
+    );
+UPDATE tides_master
+SET active = False
+FROM to_deactivate
+WHERE to_deactivate.tides_id = tides_master.tides_id;
