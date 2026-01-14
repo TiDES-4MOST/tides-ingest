c8d9d1e2057f5e95375931d93ca7abe3bc98e1be
chrisfrohmaier <cf5g09@soton.ac.uk>
2026-01-14 17:25:26 +0000

    printing to debug more clearly

--- opr4_flows/opr4_controller.py
+++ opr4_flows/opr4_controller.py
@@ -224,7 +224,9 @@ def createNewTransientin4MOST(tableIn):
     
     #print(uploadParams)
     uppedObject = st.create_transient(data=uploadParams, printout=False) 
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     print(uppedObject)
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     tableIn.loc[index,'pk_4most'] = np.int64(uppedObject['id'])
   return tableIn
 
