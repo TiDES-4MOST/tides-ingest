83c9b1375fd03df86a85cb8492bb21638ff9decf
chrisfrohmaier <cf5g09@soton.ac.uk>
2026-01-15 15:57:00 +0000

    conn not engine

--- opr4_flows/opr4_controller.py
+++ opr4_flows/opr4_controller.py
@@ -336,7 +336,7 @@ def run_opr4_workflow():
         id_ChangeState = upsertedData[['tides_id', 'pk_4most']]\
             [(upsertedData['old_status'] != upsertedData['active']) & (upsertedData['pk_4most'].notnull())]
         print('Change State',id_ChangeState)
-        upsertStaged2(upsertedData,engine) ## Upsert the recent data into the staged2 table
+        upsertStaged2(upsertedData,conn) ## Upsert the recent data into the staged2 table
         sys.exit()
         deactivate_TiDES_IDs = deactivateUnobservedTransients(conn)
 
