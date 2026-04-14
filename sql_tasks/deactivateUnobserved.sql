c0d923566557e0a6718aae1bbf518a53ae25be69
chrisfrohmaier <cf5g09@soton.ac.uk>
2026-04-14 13:14:46 +0100

    Wrapped an extra brace around the AND OR statement to avoid and infinte loop potential

--- sql_tasks/deactivateUnobserved.sql
+++ sql_tasks/deactivateUnobserved.sql
@@ -2,10 +2,12 @@ CREATE TEMP TABLE to_deactivate AS
 SELECT *
 FROM tides_master
 WHERE active = True
-    AND updated < now() - interval '5days'
-    OR (
-        rlatest > 22.5
-        and glatest > 22.5
+    AND (
+        updated < now() - interval '5days'
+        OR (
+            rlatest > 22.5
+            and glatest > 22.5
+        )
     );
 --- Update the master table to deactivate the objects
 UPDATE tides_master
